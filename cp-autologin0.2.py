4ddc0ca382a28c3bd9d7998581fd85caf66b1937
Mike Kormendy <info@mikekormendy.com>
2016-03-21 09:58:24 -0500

    Initial commit of code from https://code.google.com/archive/p/zeroshell-autologin/

---
+++ cp-autologin0.2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+# load python library
+
+from urllib import urlencode
+from urllib2 import urlopen
+from HTMLParser import HTMLParser
+from time import sleep
+
+import ConfigParser, os
+
+config = ConfigParser.ConfigParser()
+config.add_section("login")
+config.add_section("default")
+
+#read the config.ini file
+fname = open("cp-autologin.conf","r")
+config.readfp(fname)
+
+# login conf
+USERNAME = config.get("login","USERNAME")  
+PASSWORD = config.get("login","PASSWORD")
+REALM = config.get("login","REALM") 
+SERVER = config.get("login","SERVER")
+
+# default params
+PROTOCOL = config.get("default","PROTOCOL") 
+PORT = config.get("default","PORT")
+SCRIPT = config.get("default","SCRIPT")
+ZSCPRedirect = config.get("default","ZSCPRedirect")
+
+URL = PROTOCOL + '://' + SERVER + ':' + PORT + '/cgi-bin/' + SCRIPT
+RENEW_INTERVAL = 40
+
+# class to parse Captive Portal HTML
+
+class ZSParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.params = {}
+		params = { 'U' : USERNAME , 'P' : PASSWORD , 'Realm' : REALM , 'Section' : 'CPAuth' , 'Action' : 'Authenticate' , 'ZSCPRedirect' : '_:::_' }
+		http_req = urlopen(URL, urlencode(params))
+		html_content = http_req.read()
+		self.feed(html_content)
+	
+	def get_authkey(self):
+			return self.params['Authenticator'] # after parse HTML return the authenticator string
+
+	def handle_starttag(self, tag, attrs):
+		if tag == 'input' and attrs[0][1] == 'hidden': # parse only de html input and hidden tags 
+			self.params[attrs[1][1]] = attrs[2][1] 
+
+parser = ZSParser() # instantiate the class
+authkey = parser.get_authkey() # get authenticator string
+
+# http_request 1 - Section = CPGW
+
+params = { 'U' : USERNAME , 'P' : PASSWORD , 'Realm' : REALM , 'Authenticator' : authkey, 'Section' : 'CPGW' , 'Action' : 'Connect' , 'ZSCPRedirect' : '_:::_' }
+urlopen(URL, urlencode(params))
+
+# http_request 2 - Section = ClientCTRL
+
+params = { 'U' : USERNAME , 'P' : PASSWORD , 'Realm' : REALM , 'Authenticator' : authkey, 'Section' : 'ClientCTRL' , 'Action' : 'Connect' , 'ZSCPRedirect' : '_:::_' }
+urlopen(URL, urlencode(params))
+
+
+while True:
+	sleep(RENEW_INTERVAL) # wait a time in seconds to renew the connection
+	params = { 'Authenticator' : authkey, 'Section' : 'CPGW' , 'Action' : 'Renew' , 'ZSCPRedirect' : '_:::_' }
+	urlopen(URL, urlencode(params))
+
